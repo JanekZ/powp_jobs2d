1473d0af0568f9a0ed90851fb6f7bbbdfc47a0a5
Jan Zimniak <zimniak.jan@proton.me>
2026-03-25 12:45:27 +0100

    Zaimplementownie hierarchii klas Command

---
+++ src/main/java/edu/kis/powp/command/SetPositionCommand.java
@@ -0,0 +1,20 @@
+package edu.kis.powp.command;
+
+import edu.kis.powp.jobs2d.Job2dDriver;
+
+public class SetPositionCommand implements DriverCommand{
+    private Job2dDriver driver;
+    private int x;
+    private int y;
+
+
+    public SetPositionCommand(Job2dDriver driver, int x, int y){
+        this.driver = driver;
+        this.x = x;
+        this.y =y ;
+    }
+
+    public void execute(){
+        this.driver.setPosition(this.x, this.y);
+    };
+}
